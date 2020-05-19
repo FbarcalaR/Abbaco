d7acd42b1ad9d5fe87155499abe608a31be338fe
Fernando <fernandobarcalarodriguez@gmail.com>
2020-05-19 23:46:32 +0200

    sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO `cashflows`.`cash_flow_group`
+INSERT INTO `cash_flow_group`
 VALUES (0, "sueldo");
-INSERT INTO `cashflows`.`cash_flow_group`
+INSERT INTO `cash_flow_group`
 VALUES (1, "comer");
