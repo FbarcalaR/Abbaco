5c928a397bc08bb8aad36175055d15b0c9b20898
Fernando <fernandobarcalarodriguez@gmail.com>
2020-05-23 15:25:36 +0200

    cashFlows dto

--- src/main/java/abbaco/presentation/dtoModels/cashFlowDto.java
+++ src/main/java/abbaco/presentation/dtoModels/cashFlowDto.java
@@ -1,5 +1,63 @@
 package abbaco.presentation.dtoModels;
 
-public class cashFlowDto {
+import java.util.Date;
+import java.util.UUID;
     
+public class CashFlowDto {
+    private UUID id;
+    private String title;
+    private int classificationId;
+	private Date creationDate;
+    private double cashAmount;
+    
+
+    public CashFlowDto(UUID id, String title, int classificationId, Date creationDate, double cashAmount) {
+        this.id = id;
+        this.title = title;
+        this.classificationId = classificationId;
+        this.creationDate = creationDate;
+        this.cashAmount = cashAmount;
+    }
+
+    public UUID getId() {
+        return this.id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getClassificationId() {
+        return this.classificationId;
+    }
+
+    public void setClassificationId(int classificationId) {
+        this.classificationId = classificationId;
+    }
+
+    public Date getCreationDate() {
+        return this.creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public double getCashAmount() {
+        return this.cashAmount;
+    }
+
+    public void setCashAmount(double cashAmount) {
+        this.cashAmount = cashAmount;
+    }
+
+
 }
